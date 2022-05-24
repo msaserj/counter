ade0ba82dcb801a3b49747a1fbf8e1b56886246c
msa <serg.ks@gmail.com>
2022-05-24 18:03:44 +0300

    counter new counter 10 completed

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
     }
     useEffect(() => {
         checkHandler()
-    },[localMax, localMin])
+    },[localMin, localMax])
 
     // onChange
     const changeMaxValue = (max: number) => {
